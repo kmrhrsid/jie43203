2defd05242ef311375f7933c146269fb423f342a
Kamariah Rasid <94262022+kmrhrsid@users.noreply.github.com>
2024-11-03 12:48:06 +0800

    Update Association.py

--- Association.py
+++ Association.py
@@ -1,9 +1,6 @@
 import pandas as pd
 import streamlit as st
 
-
 data = pd.read_csv('https://raw.githubusercontent.com/kmrhrsid/jie43203/refs/heads/main/Crop_recommendation.csv')
-
-data
 
 st.write(data)
